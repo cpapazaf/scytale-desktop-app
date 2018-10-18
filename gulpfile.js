d57fe0ba0008cd8175b1f53252f6681bd431642c
Christos Papazafeiropoulos <c.papazafeiropoulos@klarna.com>
2018-10-18 09:39:12 +0200

    added support for dev mode

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const css   = require('gulp-css');
 
 /* Build */
 
-gulp.task('build-css', function(){
+gulp.task('build-css', () => {
     return gulp.src('src/**/*.css')
         .pipe(css())
         .pipe(gulp.dest('app/'));
@@ -27,11 +27,11 @@ gulp.task('build', ['build-css', 'build-js']);
 /* Copy */
 
 gulp.task('copy-html', () => {
-    return gulp.src('src/*.html').pipe(gulp.dest('app/'));
+    gulp.src('src/*.html').pipe(gulp.dest('app/'));
 });
 
 gulp.task('copy-assets', () => {
-    return gulp.src('assets/**/*').pipe(gulp.dest('app/assets'));
+  gulp.src('assets/**/*').pipe(gulp.dest('app/assets'));
 });
 
 gulp.task('copy', ['copy-html', 'copy-assets']);
@@ -43,6 +43,32 @@ gulp.task('copy', ['copy-html', 'copy-assets']);
 const cmd   = (name) => 'node_modules/.bin/' + name;
 const args  = (more) => Array.isArray(more) ? ['.'].concat(more) : ['.'];
 const exit  = () => process.exit();
+const electron = require('electron-connect').server.create({
+  verbose: true
+});
+
+gulp.task('restart:browser', done => {
+  electron.stop();
+  electron.restart();
+  done();
+});
+
+gulp.task('reload:renderer', done => {
+  // Reload renderer process
+  electron.reload();
+  done();
+});
+
+gulp.task('dev', () => {
+  //Start browser process
+  electron.start();
+  // Restart browser process
+  gulp.watch('src/**/*.js', ['build','restart:browser']);
+  // Reload renderer process
+  gulp.watch('src/**/*.css', ['build','reload:renderer']);
+  gulp.watch('src/**/*.html', ['build','reload:renderer']);
+  gulp.watch('src/**/*.js', ['build','reload:renderer']);
+});
 
 gulp.task('start', ['copy', 'build'], () => {
     spawn(cmd('electron'), args(), { stdio: 'inherit' }).on('close', exit);
