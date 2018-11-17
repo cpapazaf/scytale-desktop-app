fddfa3f6dc44561ec08ec7d2e55a1ae11ca83c16
Christos Papazafeiropoulos <c.papazafeiropoulos@klarna.com>
2018-11-17 23:50:15 +0100

    added little more tests

--- src/actions/AppActions.test.js
+++ src/actions/AppActions.test.js
@@ -7,18 +7,16 @@ import thunk from "redux-thunk"
 export const mockStore = configureMockStore([thunk])
 
 describe('actions/App', () => {
-    describe('setConfig', () => {
-        it('sets Config', async () => {
-            const uname = 'Bob'
-            const roomname = 'bla'
-            const serverUrl = 'http://bla'
-            const store = mockStore();
-            store.dispatch(setConfig(uname, roomname, serverUrl))
-            const setConfigAction = await getAction(store, types.SET_CONFIG)
-            expect(setConfigAction.type).toEqual(types.SET_CONFIG)
-            expect(setConfigAction.username).toEqual(uname)
-            expect(setConfigAction.chatRoomName).toEqual(roomname)
-            expect(setConfigAction.remoteServerUrl).toEqual(serverUrl)
-        })
+    it('sets Config', async () => {
+        const uname = 'Bob'
+        const roomname = 'bla'
+        const serverUrl = 'http://bla'
+        const store = mockStore();
+        store.dispatch(setConfig(uname, roomname, serverUrl))
+        const setConfigAction = await getAction(store, types.SET_CONFIG)
+        expect(setConfigAction.type).toEqual(types.SET_CONFIG)
+        expect(setConfigAction.username).toEqual(uname)
+        expect(setConfigAction.chatRoomName).toEqual(roomname)
+        expect(setConfigAction.remoteServerUrl).toEqual(serverUrl)
     })
 })
